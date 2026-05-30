8aee3b3f2fea93a57f4bb5a0b0a2ed6c849b4abd
Pruthvish05 <176687202+Pruthvish05@users.noreply.github.com>
2026-05-30 22:38:04 +0530

    segregating-2

---
+++ LnK/file_utils.py
@@ -0,0 +1,7 @@
+import os
+
+from config import ENCRYPTED_DIR, DECRYPTED_DIR, REGISTRY_FILE
+def ensure_directories():
+    os.makedirs(ENCRYPTED_DIR, exist_ok=True)
+    os.makedirs(DECRYPTED_DIR, exist_ok=True)
+    os.makedirs(os.path.dirname(REGISTRY_FILE), exist_ok=True)
